b78c7216922a4e36993493cdec43be622c094d32
prateekcsit <prateek.csit@gmail.com>
2025-12-08 20:23:26 +0530

    Update test_main.py
    added path info

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,7 @@
-from /src.main import add
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0,parent_dir)
+from src.main import add
 def test_add():
     assert add(2, 3) == 5
