0ead3ba96095a1104c8a31913605c6f7432470b7
prateekcsit <prateek.csit@gmail.com>
2025-12-09 14:44:01 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,6 @@ import sys
 import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0,parent_dir)
-from src.add import sub
+from src.app import sub
 def test_sub():
     assert sub(2, 3) == -1
